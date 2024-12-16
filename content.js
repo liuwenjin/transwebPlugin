d696de028ed5ef64369b4d8bb8f1baba603e33e0
liuwenjin <neilking@163.com>
2024-12-16 16:38:42 +0800

    解决部分浏览器点击按钮无效的问题

--- content.js
+++ content.js
@@ -5,17 +5,28 @@ let iframe = aiPluginTool.loadAiService(transweb);
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.appName) {
     let content = document.body.innerText;
-    html2canvas(document.body).then((canvas) => {
-      const iData = canvas.toDataURL("image/png");
-      let d = {
-        title: document.title,
-        url: location.href,
-        content: content,
-        appName: request.appName,
-        articleImg: iData
-      }
-      aiPluginTool.activateTask(iframe, d);
-    })
+    try {
+       html2canvas(document.body).then((canvas) => {
+         const iData = canvas.toDataURL("image/png");
+         let d = {
+           title: document.title,
+           url: location.href,
+           content: content,
+           appName: request.appName,
+           articleImg: iData
+         }
+         aiPluginTool.activateTask(iframe, d);
+       }) 
+    }
+    catch(e) {
+        let d = {
+          title: document.title,
+          url: location.href,
+          content: content,
+          appName: request.appName
+        }
+        aiPluginTool.activateTask(iframe, d);
+    }
     
   }
 });
